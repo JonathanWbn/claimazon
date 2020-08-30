a24ef38511c46786b3562d34bd7a5df3c3614e80
Jonathan Wieben <jwieben@hey.com>
2020-08-30 15:24:41 +0200

    New: Add styled layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 
 import Head from 'next/head'
 
+import Layout from '../components/layout'
+
 type Props = {
   pageProps: { [key: string]: unknown }
   Component: React.ComponentType
@@ -13,8 +15,14 @@ function MyApp({ Component, pageProps }: Props): React.ReactElement {
       <Head>
         <title>Claimazon</title>
         <link rel="icon" href="/claimsforce-thumbnail.png" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   )
 }
