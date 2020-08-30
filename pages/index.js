a8177b41c4e26649a727555dc1093ef9e6a82b4b
Jonathan Wieben <jwieben@hey.com>
2020-08-30 08:52:52 +0200

    Chore: Setup basic next app

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Claimazon</title>
+        <link rel="icon" href="/claimsforce-thumbnail.png" />
+      </Head>
+      <h1>Claimazon - It's still claim one.</h1>
+    </>
+  );
+}
