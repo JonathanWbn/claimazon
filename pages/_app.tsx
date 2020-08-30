a04b87e5be5d884da45504a7f6f6ab48320ebe42
Jonathan Wieben <jwieben@hey.com>
2020-08-30 12:53:20 +0200

    Chore: Centralize head tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import '../styles/globals.css'
 
+import Head from 'next/head'
+
 type Props = {
   pageProps: { [key: string]: unknown }
   Component: React.ComponentType
 }
 
 function MyApp({ Component, pageProps }: Props): React.ReactElement {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Claimazon</title>
+        <link rel="icon" href="/claimsforce-thumbnail.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,5 @@
-import Head from 'next/head'
-
 import ProductList from '../components/product-list'
 
 export default function Home(): React.ReactElement {
-  return (
-    <>
-      <Head>
-        <title>Claimazon</title>
-        <link rel="icon" href="/claimsforce-thumbnail.png" />
-      </Head>
-      <ProductList />
-    </>
-  )
+  return <ProductList />
 }
